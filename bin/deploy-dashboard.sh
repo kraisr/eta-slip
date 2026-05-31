90cc928319f41ddf7c76b9be5f55c1f6fffbc6ff
Ubuntu <ubuntu@ip-172-31-10-27.us-east-2.compute.internal>
2026-05-31 20:06:43 +0000

    chore: add timeout to deploy script

--- bin/deploy-dashboard.sh
+++ bin/deploy-dashboard.sh
@@ -6,5 +6,5 @@ cd /srv/eta-slip
 git fetch origin main
 git reset --hard origin/main
 
-docker compose up -d --build --no-deps dashboard
+timeout 600 docker compose up -d --build --no-deps dashboard
 docker image prune -f
